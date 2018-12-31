14ea07e7c14cda4cc682150115c8ac48284eb838
Manne Öhrström <manne.ohrstrom@autodesk.com>
2018-12-31 01:30:49 +0000

    comments on PR

---
+++ scripts/ci_github_comment.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e # halt script on error
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
+   # hey that's a pull request
+    curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
+    -d "{\"body\": \"Hello world\"}" \
+    "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+fi
