027f9e2be095ccf4091ed9952bd8df03e5481d24
Manne Öhrström <manne.ohrstrom@autodesk.com>
2018-12-31 10:26:17 +0000

    S3 and python test

--- scripts/ci_preview.sh
+++ scripts/ci_preview.sh
@@ -3,9 +3,15 @@ set -e # halt script on error
 
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
-   # hey that's a pull request
+
+    # upload to S3
+    python ./ci_s3_upload.py
+
+    # add comment to PR
     curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
-    -d "{\"body\": \"Hello world\"}" \
+    -d "{\"body\": \"Doc Preview here: https://foo.bar/sgpipelinetest/${TRAVIS_BUILD_NUMBER}/index.html\"}" \
     "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+
+
 fi
 
---
+++ scripts/ci_s3_upload.py
@@ -0,0 +1,7 @@
+print "hello python world"
+
+import boto3
+import os
+import sys
+
+print boto3
