c9dbfbe132ca240fbc3b6955f72611948da4df6e
Manne Öhrström <manne.ohrstrom@autodesk.com>
2019-01-02 11:59:43 +0000

    wip

--- scripts_docker/build_docs.sh
+++ scripts_docker/build_docs.sh
@@ -6,17 +6,15 @@ if [ ! -f /.dockerenv ]; then
     exit 1
 fi
 
-ROOT_FOLDER=/app
-BUILD_FOLDER=${ROOT_FOLDER}/_build
-
-
 # exit on error
 set -e
 
+ROOT_FOLDER=/app
+BUILD_FOLDER=${ROOT_FOLDER}/_build
+WEBSITE_FOLDER=${BUILD_FOLDER}/jekyll
+
 echo "------------------------------------------------------"
 echo "doc build process starting"
-echo `python --version`
-echo `ruby --version`
 echo "------------------------------------------------------"
 
 echo "cleaning out build location"
@@ -29,22 +27,18 @@ mkdir ${BUILD_FOLDER}/markdown_src
 echo "copying markdown docs scaffold into build locatio"
 cp -r ${ROOT_FOLDER}/docs/* ${BUILD_FOLDER}/markdown_src
 
-echo "running python script compile"
-#python ${ROOT_FOLDER}/scripts_docker/generate_sphinx_markdown.py
-
+echo "cloning core..."
 git clone --depth 1 https://github.com/shotgunsoftware/tk-core.git ${BUILD_FOLDER}/git/tk-core
 export PYTHONPATH=$PYTHONPATH:${BUILD_FOLDER}/git/tk-core/python
 
-sphinx-build -h
-
-sphinx-build -b markdown -c ${ROOT_FOLDER}/sphinx -D project='foo' -D release='bar' -D version='0.0.0' ${BUILD_FOLDER}/git/tk-core/docs ${BUILD_FOLDER}/sphinx/tk-core
-
+echo "converting sphinx -> markdown"
+sphinx-build -b markdown -c ${ROOT_FOLDER}/sphinx -D project='foo' -D release='bar' -D version='0.0.0' ${BUILD_FOLDER}/git/tk-core/docs ${BUILD_FOLDER}/markdown_src/tk-core
 
 echo "building jekyll site"
-
-bundle exec jekyll build --help
-
-JEKYLL_ENV=production bundle exec jekyll build --config ${ROOT_FOLDER}/_config.yml --source ${BUILD_FOLDER}/markdown_src --destination ${BUILD_FOLDER}/jekyll
+JEKYLL_ENV=production bundle exec jekyll build --config ${ROOT_FOLDER}/jekyll/_config.yml --source ${BUILD_FOLDER}/markdown_src --destination ${WEBSITE_FOLDER}
 
 
+echo "------------------------------------------------------"
+echo "All done. Build in ${WEBSITE_FOLDER}"
+echo "------------------------------------------------------"
 
