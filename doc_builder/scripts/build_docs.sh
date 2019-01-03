17369132459c9d5a4fbbe3ce6193eb30c222727e
Manne Öhrström <manne.ohrstrom@autodesk.com>
2019-01-03 17:29:11 +0000

    wip

---
+++ doc_builder/scripts/build_docs.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# exit on error
+set -e
+
+for i in "$@"
+do
+case $i in
+    -u=*|--url=*)
+    URL="${i#*=}"
+    shift # past argument=value
+    ;;
+    -p=*|--url-path=*)
+    URLPATH="${i#*=}"
+    shift # past argument=value
+    ;;
+    -s=*|--source=*)
+    SOURCE="${i#*=}"
+    shift # past argument=value
+    ;;
+    -o=*|--output=*)
+    OUTPUT="${i#*=}"
+    shift # past argument=value
+    ;;
+    *)
+          # unknown option
+    ;;
+esac
+done
+
+echo "---------------------------------------------------"
+echo "DOC BUILD"
+echo "Source = ${SOURCE}"
+echo "Output = ${OUTPUT}"
+echo "Url    = ${URL}${URLPATH}"
+echo "---------------------------------------------------"
+
+THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+TMP_BUILD_FOLDER=${THIS_DIR}/_build
+WEBSITE_FOLDER=${OUTPUT}
+
+echo "cleaning out build location"
+rm -rf ${TMP_BUILD_FOLDER}/_build
+
+echo "creating build location"
+mkdir ${TMP_BUILD_FOLDER}
+mkdir ${TMP_BUILD_FOLDER}/markdown_src
+
+echo "copying markdown docs scaffold into build locatio"
+cp -r ${SOURCE}/* ${TMP_BUILD_FOLDER}/markdown_src
+
+echo "cloning core..."
+git clone --depth 1 https://github.com/shotgunsoftware/tk-core.git ${TMP_BUILD_FOLDER}/git/tk-core
+export PYTHONPATH=$PYTHONPATH:${TMP_BUILD_FOLDER}/git/tk-core/python
+
+echo "converting sphinx -> markdown"
+sphinx-build -b markdown -c ${THIS_DIR}/../sphinx -D project='foo' -D release='bar' -D version='0.0.0' ${TMP_BUILD_FOLDER}/git/tk-core/docs ${TMP_BUILD_FOLDER}/markdown_src/tk-core
+
+echo "building jekyll site"
+JEKYLL_ENV=production bundle exec jekyll build --config ${THIS_DIR}/../jekyll/_config.yml --source ${TMP_BUILD_FOLDER}/markdown_src --destination ${OUTPUT}
+
+
+echo "------------------------------------------------------"
+echo "Build completed."
+echo "------------------------------------------------------"
+
---
+++ scripts/travis-generate-docs.py
@@ -0,0 +1,94 @@
+import boto3
+import os
+import sys
+
+print "Begin travis doc generation"
+
+S3_BUCKET = "doctest3"
+S3_WEB_URL = "http://doctest3.s3-website.eu-west-2.amazonaws.com"
+S3_PATH = "/travis-doc-build/{commit}".format(commit=os.environ["TRAVIS_COMMIT"])
+
+GITHUB_PAGES_URL = "https://manneohrstrom.github.io"
+GITHUB_PAGES_PATH = "/jekyll-test"
+
+
+def upload_folder_to_s3(bucket, src, dst):
+    """
+    Upload folder to S3
+    """
+    names = os.listdir(src)
+    for name in names:
+
+        srcname = os.path.join(src, name)
+        dstname = os.path.join(dst, name)
+
+        if os.path.isdir(srcname):
+            upload_folder_to_s3(bucket, srcname, dstname)
+        else:
+            print "upload '{}' -> '{}'".format(srcname, dstname)
+            bucket.upload_file(srcname, dstname)
+
+
+root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+doc_script = os.path.join(root_path, "doc_builder", "scripts", "build_docs.sh")
+output_path = os.path.join(root_path, "_build")
+source_path = os.path.join(root_path, "docs")
+
+
+if os.environ.get("TRAVIS_PULL_REQUEST") == "true":
+
+    print "Inside a pull request - will upload preview to S3"
+
+    s3_full_url = "{url}{path}/index.html".format(url=S3_WEB_URL, path=S3_PATH)
+
+    # build the doc
+    doc_command = "{script} --url={url} --url-path={path} --source={source} --output={output}".format(
+        script=doc_script,
+        url=S3_URL,
+        url-path=s3_PATH,
+        source=source_path,
+        output=output_path,
+    )
+
+    print "Building docs: '{}'".format(doc_command)
+    if os.system(doc_command) != 0:
+        sys.exit(1)
+
+    print 'uploading to s3...'
+    s3_client = boto3.client(
+        's3',
+        aws_access_key_id=os.environ["AWS_S3_ACCESS_KEY"],
+        aws_secret_access_key=os.environ["AWS_S3_ACCESS_TOKEN"]
+    )
+    s3_bucket = s3_client.Bucket(S3_BUCKET)
+    upload_folder_to_s3(s3_bucket, output_path, S3_PATH)
+
+    print 'generating pull request comment...'
+    cmd =  "curl -H 'Authorization: token {token}' -X POST ".format(token=os.environ["GITHUB_TOKEN"])
+    cmd += "-d '{\"body\": \"Documentation here: {url}\"}' ".format(url=s3_full_url)
+    cmd += "'https://api.github.com/repos/{repo_slug}/issues/{pull_request}/comments'".format(
+        repo_slug=os.environ["TRAVIS_REPO_SLUG"],
+        pull_request=os.environ["TRAVIS_PULL_REQUEST"]
+    )
+    os.system(cmd)
+
+
+else:
+
+    print "On master branch - will build for gh-pages branch"
+
+    doc_command = "{script} --url={url} --url-path={path} --source={source} --output={output}".format(
+        script=doc_script,
+        url=GITHUB_PAGES_URL,
+        url-path=GITHUB_PAGES_PATH,
+        source=source_path,
+        output=output_path
+    )
+    print "Building docs: '{}'".format(doc_command)
+    if os.system(doc_command) != 0:
+        sys.exit(1)
+
+print "doc generation completed successfully."
+
+
+
