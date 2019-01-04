a5d334d2e4a88b2eedc53a3d9fc0d1791c4ad958
Manne Öhrström <manne.ohrstrom@autodesk.com>
2019-01-04 11:33:46 +0000

    included qt in docker image

--- doc_builder/scripts/build_sphinx.py
+++ doc_builder/scripts/build_sphinx.py
@@ -1,1 +1,7 @@
-print "hello from python script"
+import sys
+import os
+
+input_folder = sys.argv[1]
+print "Sphinx doc generation"
+print "will process sphinx structure in %s" % input_folder
+print ""
