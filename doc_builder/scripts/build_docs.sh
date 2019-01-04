b2e686c55a3818db18d17c7fb3861b2c6dfd022a
Manne Öhrström <manne.ohrstrom@autodesk.com>
2019-01-04 10:41:02 +0000

    test

--- doc_builder/scripts/build_docs.sh
+++ doc_builder/scripts/build_docs.sh
@@ -37,7 +37,7 @@ echo "---------------------------------------------------"
 
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-TMP_BUILD_FOLDER=${THIS_DIR}/../_build
+TMP_BUILD_FOLDER=${THIS_DIR}/../_build/markdown_src
 WEBSITE_FOLDER=${OUTPUT}
 
 echo "cleaning out internal build location '${TMP_BUILD_FOLDER}'..."
@@ -47,21 +47,39 @@ echo "cleaning out final build location '${WEBSITE_FOLDER}'..."
 rm -rf ${WEBSITE_FOLDER}
 
 echo "creating build location"
-mkdir ${TMP_BUILD_FOLDER}
-mkdir ${TMP_BUILD_FOLDER}/markdown_src
+mkdir -p ${TMP_BUILD_FOLDER}
 
-echo "copying markdown docs scaffold into build locatio"
-cp -r ${SOURCE}/* ${TMP_BUILD_FOLDER}/markdown_src
+echo "copying markdown docs scaffold into build location"
+cp -r ${SOURCE}/* ${TMP_BUILD_FOLDER}
+
+echo "running sphinx builds..."
+python {THIS_DIR}/build_sphinx.py ${TMP_BUILD_FOLDER}
+
 
 echo "cloning core..."
-git clone --depth 1 https://github.com/shotgunsoftware/tk-core.git ${TMP_BUILD_FOLDER}/git/tk-core
-export PYTHONPATH=$PYTHONPATH:${TMP_BUILD_FOLDER}/git/tk-core/python
+TMP_SPHINX_FOLDER=${TMP_BUILD_FOLDER}_spx
+rm -rf ${TMP_SPHINX_FOLDER}
+mkdir -p ${TMP_SPHINX_FOLDER}
+
+git clone --depth 1 https://github.com/shotgunsoftware/tk-core.git ${TMP_SPHINX_FOLDER}/git/tk-core
+export PYTHONPATH=$PYTHONPATH:${TMP_SPHINX_FOLDER}/git/tk-core/python
 
 echo "converting sphinx -> markdown"
-sphinx-build -b markdown -c ${THIS_DIR}/../sphinx -D project='foo' -D release='bar' -D version='0.0.0' ${TMP_BUILD_FOLDER}/git/tk-core/docs ${TMP_BUILD_FOLDER}/markdown_src/tk-core
+sphinx-build -b markdown -c ${THIS_DIR}/../sphinx -D project='foo' -D release='bar' -D version='0.0.0' ${TMP_SPHINX_FOLDER}/git/tk-core/docs ${TMP_BUILD_FOLDER}/tk-core
+
+git clone --depth 1 https://github.com/shotgunsoftware/tk-framework-shotgunutils.git ${TMP_SPHINX_FOLDER}/git/tk-framework-shotgunutils
+export PYTHONPATH=$PYTHONPATH:${TMP_SPHINX_FOLDER}/git/tk-framework-shotgunutils/python
+
+echo "converting sphinx -> markdown"
+sphinx-build -b markdown -c ${THIS_DIR}/../sphinx -D project='foo' -D release='bar' -D version='0.0.0' ${TMP_SPHINX_FOLDER}/git/tk-framework-shotgunutils/docs ${TMP_BUILD_FOLDER}/tk-framework-shotgunutils
+
+
 
 echo "building jekyll site"
-BUNDLE_GEMFILE=${THIS_DIR}/../Gemfile JEKYLL_ENV=production bundle exec jekyll build --baseurl ${URLPATH} --config ${THIS_DIR}/../jekyll/_config.yml --source ${TMP_BUILD_FOLDER}/markdown_src --destination ${OUTPUT}
+BUNDLE_GEMFILE=${THIS_DIR}/../Gemfile JEKYLL_ENV=production \
+bundle exec jekyll build \
+--baseurl ${URLPATH} --config ${THIS_DIR}/../jekyll/_config.yml \
+--source ${TMP_BUILD_FOLDER} --destination ${OUTPUT}
 
 
 echo "------------------------------------------------------"
